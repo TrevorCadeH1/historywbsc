f852ab11f475d7d0d8c1a18acf7f6530d1aa402d
Trevor Hutchison <trevor.hutchison@wurthbsc.com>
2025-07-09 14:21:53 -0500

    first

---
+++ src/app/historywbsc/page.tsx
@@ -0,0 +1,8 @@
+export default function HistoryWBSCPage() {
+    return (
+        <div className="responsive-max-width min-h-screen flex flex-col">
+            <div className="flex-1 flex items-center justify-center p-8">
+                </div>
+        </div>
+    )
+}
